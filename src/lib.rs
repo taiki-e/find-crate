559635848925add7b03c36748410c02fa929c924
Taiki Endo <te316e89@gmail.com>
2019-09-08 18:47:08 +0900

    Deny rust_2018_idioms and single_use_lifetimes for doc tests

--- src/lib.rs
+++ src/lib.rs
@@ -71,7 +71,10 @@
 //! Also, [`find_crate()`] and [`Manifest::new()`] read `Cargo.toml` in `CARGO_MANIFEST_DIR` as manifest.
 
 #![doc(html_root_url = "https://docs.rs/find-crate/0.4.0")]
-#![doc(test(attr(deny(warnings), allow(dead_code, unused_assignments, unused_variables))))]
+#![doc(test(
+    no_crate_inject,
+    attr(deny(warnings, rust_2018_idioms, single_use_lifetimes), allow(dead_code))
+))]
 #![warn(missing_docs, missing_debug_implementations)]
 #![warn(unsafe_code)]
 #![warn(rust_2018_idioms, unreachable_pub)]
