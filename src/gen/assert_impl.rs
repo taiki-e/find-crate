da4a950b4cbbf8adc92294276e410d9a4476b470
Taiki Endo <te316e89@gmail.com>
2023-01-11 03:00:24 +0900

    codegen: Generate assertions for UnwindSafe/RefUnwindSafe impl

--- src/gen/assert_impl.rs
+++ src/gen/assert_impl.rs
@@ -4,10 +4,80 @@
 
 #![cfg_attr(rustfmt, rustfmt::skip)]
 
+#![allow(clippy::std_instead_of_alloc, clippy::std_instead_of_core)]
+#[allow(unused_imports)]
+use core::marker::PhantomPinned;
+/// `Send` & `!Sync`
+#[allow(dead_code)]
+struct NotSync(core::cell::Cell<()>);
+/// `!Send` & `!Sync`
+#[allow(dead_code)]
+struct NotSendSync(std::rc::Rc<()>);
+/// `!UnwindSafe`
+#[allow(dead_code)]
+struct NotUnwindSafe(&'static mut ());
+/// `!RefUnwindSafe`
+#[allow(dead_code)]
+struct NotRefUnwindSafe(core::cell::UnsafeCell<()>);
+#[allow(dead_code)]
+fn assert_send<T: ?Sized + Send>() {}
+#[allow(dead_code)]
+fn assert_sync<T: ?Sized + Sync>() {}
+#[allow(dead_code)]
+fn assert_unpin<T: ?Sized + Unpin>() {}
+#[allow(dead_code)]
+fn assert_unwind_safe<T: ?Sized + std::panic::UnwindSafe>() {}
+#[allow(dead_code)]
+fn assert_ref_unwind_safe<T: ?Sized + std::panic::RefUnwindSafe>() {}
+#[allow(unused_macros)]
+macro_rules! assert_not_send {
+    ($ty:ty) => {
+        static_assertions::assert_not_impl_all!($ty : Send);
+    };
+}
+#[allow(unused_macros)]
+macro_rules! assert_not_sync {
+    ($ty:ty) => {
+        static_assertions::assert_not_impl_all!($ty : Sync);
+    };
+}
+#[allow(unused_macros)]
+macro_rules! assert_not_unpin {
+    ($ty:ty) => {
+        static_assertions::assert_not_impl_all!($ty : Unpin);
+    };
+}
+#[allow(unused_macros)]
+macro_rules! assert_not_unwind_safe {
+    ($ty:ty) => {
+        static_assertions::assert_not_impl_all!($ty : std::panic::UnwindSafe);
+    };
+}
+#[allow(unused_macros)]
+macro_rules! assert_not_ref_unwind_safe {
+    ($ty:ty) => {
+        static_assertions::assert_not_impl_all!($ty : std::panic::RefUnwindSafe);
+    };
+}
 const _: fn() = || {
-    fn assert_auto_traits<T: ?Sized + Send + Sync + Unpin>() {}
-    assert_auto_traits::<crate::error::Error>();
-    assert_auto_traits::<crate::Dependencies>();
-    assert_auto_traits::<crate::Package>();
-    assert_auto_traits::<crate::Manifest>();
+    assert_send::<crate::error::Error>();
+    assert_sync::<crate::error::Error>();
+    assert_unpin::<crate::error::Error>();
+    assert_not_unwind_safe!(crate::error::Error);
+    assert_not_ref_unwind_safe!(crate::error::Error);
+    assert_send::<crate::Dependencies>();
+    assert_sync::<crate::Dependencies>();
+    assert_unpin::<crate::Dependencies>();
+    assert_unwind_safe::<crate::Dependencies>();
+    assert_ref_unwind_safe::<crate::Dependencies>();
+    assert_send::<crate::Package>();
+    assert_sync::<crate::Package>();
+    assert_unpin::<crate::Package>();
+    assert_unwind_safe::<crate::Package>();
+    assert_ref_unwind_safe::<crate::Package>();
+    assert_send::<crate::Manifest>();
+    assert_sync::<crate::Manifest>();
+    assert_unpin::<crate::Manifest>();
+    assert_unwind_safe::<crate::Manifest>();
+    assert_ref_unwind_safe::<crate::Manifest>();
 };
