59743d9219e4ee1a7945ca881605337664168c6f
Taiki Endo <te316e89@gmail.com>
2024-01-27 02:04:46 +0900

    codegen: Cleanup

--- src/gen/assert_impl.rs
+++ src/gen/assert_impl.rs
@@ -4,42 +4,37 @@
 // It is not intended for manual editing.
 
 #![cfg_attr(rustfmt, rustfmt::skip)]
-#![allow(clippy::std_instead_of_alloc, clippy::std_instead_of_core)]
-#[allow(dead_code)]
+#![allow(
+    dead_code,
+    unused_macros,
+    clippy::std_instead_of_alloc,
+    clippy::std_instead_of_core,
+)]
 fn assert_send<T: ?Sized + Send>() {}
-#[allow(dead_code)]
 fn assert_sync<T: ?Sized + Sync>() {}
-#[allow(dead_code)]
 fn assert_unpin<T: ?Sized + Unpin>() {}
-#[allow(dead_code)]
 fn assert_unwind_safe<T: ?Sized + std::panic::UnwindSafe>() {}
-#[allow(dead_code)]
 fn assert_ref_unwind_safe<T: ?Sized + std::panic::RefUnwindSafe>() {}
-#[allow(unused_macros)]
 macro_rules! assert_not_send {
     ($ty:ty) => {
         static_assertions::assert_not_impl_all!($ty : Send);
     };
 }
-#[allow(unused_macros)]
 macro_rules! assert_not_sync {
     ($ty:ty) => {
         static_assertions::assert_not_impl_all!($ty : Sync);
     };
 }
-#[allow(unused_macros)]
 macro_rules! assert_not_unpin {
     ($ty:ty) => {
         static_assertions::assert_not_impl_all!($ty : Unpin);
     };
 }
-#[allow(unused_macros)]
 macro_rules! assert_not_unwind_safe {
     ($ty:ty) => {
         static_assertions::assert_not_impl_all!($ty : std::panic::UnwindSafe);
     };
 }
-#[allow(unused_macros)]
 macro_rules! assert_not_ref_unwind_safe {
     ($ty:ty) => {
         static_assertions::assert_not_impl_all!($ty : std::panic::RefUnwindSafe);
